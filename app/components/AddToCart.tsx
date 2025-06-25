c7d109428d0ad95667351f218f913efc3432735b
Subu114 <21co63@aitdgoa.edu.in>
2025-06-25 14:16:09 +0530

    Client and Server Pages

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+'use client';   
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        AddToCart
+        <button onClick={() => console.log("hello")}>Click to log</button>
+    </div>
+  )
+}
+
+export default AddToCart
